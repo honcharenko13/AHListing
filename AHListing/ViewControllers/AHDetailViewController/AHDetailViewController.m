bbf46d69e77bf7486270cb4b9dfdfeb4d467ff4f
Mac <honcharenko13@redwerk.com>
2017-01-25 01:26:45 +0200

    added impossibility to add same item to saved;

--- AHListing/ViewControllers/AHDetailViewController/AHDetailViewController.m
+++ AHListing/ViewControllers/AHDetailViewController/AHDetailViewController.m
@@ -11,9 +11,12 @@
 #import <SDWebImage/UIImageView+WebCache.h>
 #import "AHParsedListing.h"
 #import "ListingItem+CoreDataClass.h"
+#import "AHAlertManager.h"
 
 static NSString *kAHDetailViewControllerDeleteTitleName = @"Delete From Saved";
 static NSString *kAHDetailViewControllerTitle = @"Details";
+static NSString *kAHDetailViewControllerAlertTitle = @"Error";
+static NSString *kAHDetailViewControllerAlertMessage = @"You have already had this item in saved!";
 
 @interface AHDetailViewController ()
 
@@ -23,6 +26,8 @@ static NSString *kAHDetailViewControllerTitle = @"Details";
 @property (weak, nonatomic) IBOutlet UILabel *descriptionLabel;
 @property (weak, nonatomic) IBOutlet UIButton *actionButton;
 
+@property (nonatomic) NSArray *savedListingsArray;
+
 @end
 
 @implementation AHDetailViewController
@@ -30,12 +35,19 @@ static NSString *kAHDetailViewControllerTitle = @"Details";
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.savedListingsArray = [NSArray array];
     self.navigationItem.title = kAHDetailViewControllerTitle;
     [self fillInformation];
     if (self.isSaved) {
         [self.actionButton setTitle:kAHDetailViewControllerDeleteTitleName forState:UIControlStateNormal];
         [self.actionButton setBackgroundColor:[UIColor redColor]];
     }
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    self.savedListingsArray = [ListingItem MR_findAll];
 }
 
 #pragma mark - Helpers
@@ -57,21 +69,33 @@ static NSString *kAHDetailViewControllerTitle = @"Details";
         [self.currentSavedListing MR_deleteEntity];
         [self.navigationController popViewControllerAnimated:YES];
     } else {
-        NSManagedObjectContext *defaultContext = [NSManagedObjectContext MR_defaultContext];
-        ListingItem *listing = [ListingItem MR_createEntity];
-        [MagicalRecord saveWithBlock:^(NSManagedObjectContext * _Nonnull localContext) {
-            listing.name = self.currentParsedListing.name;
-            listing.listingDescription = self.currentParsedListing.listingDescription;
-            listing.price = self.currentParsedListing.price;
-            listing.fullImageUrl = self.currentParsedListing.imageFulllUrl;
-            listing.thumbnailImageUrl = self.currentParsedListing.imageThumbnailUrl;
-            listing.listingId = self.currentParsedListing.listingId.doubleValue;
-            [defaultContext MR_saveToPersistentStoreWithCompletion:^(BOOL contextDidSave, NSError * _Nullable error) {
-                if (contextDidSave) {
-                    [self.navigationController popViewControllerAnimated:YES];
-                }
+        BOOL isAlreadyInSaved = NO;
+        for (ListingItem *listing in self.savedListingsArray) {
+            if (listing.listingId == self.currentParsedListing.listingId.doubleValue) {
+                isAlreadyInSaved = YES;
+            }
+        }
+        if (isAlreadyInSaved) {
+            [AHAlertManager createAlertWithTitle:kAHDetailViewControllerAlertTitle
+                                         message:kAHDetailViewControllerAlertMessage
+                                  viewController:self];
+        } else {
+            NSManagedObjectContext *defaultContext = [NSManagedObjectContext MR_defaultContext];
+            ListingItem *listing = [ListingItem MR_createEntity];
+            [MagicalRecord saveWithBlock:^(NSManagedObjectContext * _Nonnull localContext) {
+                listing.name = self.currentParsedListing.name;
+                listing.listingDescription = self.currentParsedListing.listingDescription;
+                listing.price = self.currentParsedListing.price;
+                listing.fullImageUrl = self.currentParsedListing.imageFulllUrl;
+                listing.thumbnailImageUrl = self.currentParsedListing.imageThumbnailUrl;
+                listing.listingId = self.currentParsedListing.listingId.doubleValue;
+                [defaultContext MR_saveToPersistentStoreWithCompletion:^(BOOL contextDidSave, NSError * _Nullable error) {
+                    if (contextDidSave) {
+                        [self.navigationController popViewControllerAnimated:YES];
+                    }
+                }];
             }];
-        }];
+        }
     }
 }
 
